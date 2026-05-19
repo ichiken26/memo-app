3489123539f3bd52056f026b249a69a748557338
ichiken <62ichiken@gmail.com>
2026-05-19 22:00:48 +0900

    changed Tab name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  app: {
+    head: {
+      title: 'MEMO APP'
+    }
+  },
   devtools: { enabled: process.env.NUXT_DEVTOOLS === 'true' },
   nitro: {
     preset: 'cloudflare-module',
